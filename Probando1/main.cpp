1eb7112d4548970239ddf90cae4418c13a1727b9
Mahahein <mahahein3@gmail.com>
2015-01-05 10:50:27 -0300

    primer commit

---
+++ Probando1/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Maha
+ *
+ * Created on 5 de enero de 2015, 10:42 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hola Mundo" << endl;
+    
+    return 0;
+}
+
